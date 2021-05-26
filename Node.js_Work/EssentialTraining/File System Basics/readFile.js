ee98e0a1c526104238142c371d28aeb2b86ab5bf
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 13:44:27 -0400

    Error handling

--- Node.js_Work/EssentialTraining/File System Basics/readFile.js
+++ Node.js_Work/EssentialTraining/File System Basics/readFile.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 
-const text = fs.readFileSync('./assets/Readme.md', 'utf-8');
-console.log(text);
+//const text = fs.readFileSync('./assets/Readme.md', 'utf-8');
+//console.log(text);
 
 console.log('-------------------------------------------\n');
 
-fs.readFile('./assets/Readme.md', 'utf-8', (err, text) => {
+fs.readFile('./assets/patrick.jpg', (err, img) => {
+
+    if (err) {
+        console.log(`Error: ${err}`);
+        process.exit();
+    }
 
     console.log('File contents read.');
-    console.log(text);
+    console.log(img);
 
 });
