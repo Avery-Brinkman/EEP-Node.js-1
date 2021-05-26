e08a3c759aa50fe204aa1ef70a15f74e52353f7c
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 12:08:08 -0400

    Added functions

--- Node.js_Work/EssentialTraining/producer.js
+++ Node.js_Work/EssentialTraining/producer.js
@@ -1,1 +1,12 @@
-module.exports = 'Avery'
+let count = 0;
+
+const inc = () => ++count;
+const dec = () => --count;
+
+const getCount = () => count;
+
+module.exports = {
+  inc,
+  dec,
+  getCount  
+};
