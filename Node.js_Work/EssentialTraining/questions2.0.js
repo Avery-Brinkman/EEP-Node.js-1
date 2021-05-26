5e1e42521417e7a3ef492452590e84b7c386b0fe
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 11:56:08 -0400

    Defined collectAnswers function

--- Node.js_Work/EssentialTraining/questions2.0.js
+++ Node.js_Work/EssentialTraining/questions2.0.js
@@ -1,8 +1,30 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
 const questions = [
     'What is your name? ',
     'Where do you live? ',
     'What are you doing? '
 ]
+
+const collectAnswers = (questions, doneCallback) => {
+    const answers = []
+    const [firstQ] = questions
+
+    const questionAnswered = (answer) => {
+        answers.push(answer)
+        if (answers.length < questions.length){
+            rl.question(questions[answers.length], questionAnswered)
+        } else {
+            doneCallback(answers)
+        }
+    }
+
+    rl.question(firstQ, questionAnswered)
+}
 
 collectAnswers(questions, (answers) => {
     console.log('Thanks for answers!')
