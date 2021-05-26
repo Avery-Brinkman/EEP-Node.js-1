33cf46c9efe9303b710aaf3471df5da3ba74ee77
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 13:41:05 -0400

    Formatting

--- Node.js_Work/EssentialTraining/File System Basics/readFile.js
+++ Node.js_Work/EssentialTraining/File System Basics/readFile.js
@@ -3,8 +3,11 @@ const fs = require('fs');
 const text = fs.readFileSync('./assets/Readme.md', 'utf-8');
 console.log(text);
 
+console.log('-------------------------------------------\n');
 
 fs.readFile('./assets/Readme.md', 'utf-8', (err, text) => {
-    
+
+    console.log('File contents read.');
     console.log(text);
+
 });
