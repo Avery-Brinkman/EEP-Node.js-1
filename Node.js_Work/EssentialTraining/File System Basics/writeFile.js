3c98597627a532790d56533a2e2cb3839109db68
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 13:48:49 -0400

    Created writeFile.js

---
+++ Node.js_Work/EssentialTraining/File System Basics/writeFile.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const md = `
+
+# This is a new file.
+
+We can write text to a new file w/ fs.writeFile
+
+* fs.readdir
+* fs.readFile
+* fs.writeFile
+
+`;
+
+fs.watchFile('./assets/notes.md', md.trim(), (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('File saved!');
+})
