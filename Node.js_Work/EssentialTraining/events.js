42da43df215bf9a78deb93b675d9ab778a79fc8c
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 12:39:01 -0400

    Added terminal input

--- Node.js_Work/EssentialTraining/events.js
+++ Node.js_Work/EssentialTraining/events.js
@@ -5,9 +5,18 @@ const emitter = new events.EventEmitter();
 /* Handler
  * emitter.on(nameToListenFor, callbackToRun) */
 emitter.on('customEvent', (message, user) => {
-    console.log(`${user}: ${message}`)
-})
+    console.log(`${user}: ${message}`);
+});
 
 // emitter.emit('name', dataToBePassedToHandler)
 emitter.emit('customEvent', 'Hello World!', 'Computer');
 emitter.emit('customEvent', 'yo im live', 'Matt');
+
+process.stdin.on('data', (data) => {
+    const input = data.toString().trim();
+    if (input == 'exit'){
+        emitter.emit('customEvent', 'Goodbye!', 'Process');
+        process.exit();
+    }
+    emitter.emit('customEvent', input, 'Terminal');
+});
