0efc51ffe1f940dac956bc75f329256b19f3bdd8
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 14:34:02 -0400

    Created rename.js

--- Node.js_Work/EssentialTraining/File System Basics/rename.js
+++ Node.js_Work/EssentialTraining/File System Basics/rename.js
@@ -1,17 +1,34 @@
 const fs = require('fs');
 
-/* fs.rename and fs.renameSync
- *  fs.rename('currentPath', 'newPath', callback)
- * fs.rename can also be used to move files. */
-fs.rename('./assets/colors.md', './assets/colorsRenamed.md', (err) => {
-    if (err){
-        throw err;
+// Ensures colors.md exists at start
+fs.access('./assets/colors.md', fs.F_OK, (err) => {
+    if (err) {
+        fs.writeFileSync('./assets/colors.md','utf-8');
     }
+
+    // Timer so you can see colors created and renamed
+    setTimeout(() => {
+        /* fs.rename and fs.renameSync
+        *  fs.rename('currentPath', 'newPath', callback)
+        * fs.rename can also be used to move files. */
+        fs.rename('./assets/colors.md', './assets/colorsRenamed.md', (err) => {
+            if (err){
+                console.log('Rename');
+                throw err;
+            }
+
+        })
+    }, 1000)
 })
 
 setTimeout(() => {
 
     // fs.unlink removes files, has sync and async
-    fs.unlink('./assets/colo')
+    fs.unlink('./assets/colorsRenamed.md', (err) => {
+        if (err) {
+            console.log('Unlink')
+            throw err;
+        }
+    })
 
 }, 4000);
