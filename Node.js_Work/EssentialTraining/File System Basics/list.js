ed3ed6d0722ca0ae7f43fe709454ec8bc5c9bade
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 13:31:44 -0400

    Added async fileread

--- Node.js_Work/EssentialTraining/File System Basics/list.js
+++ Node.js_Work/EssentialTraining/File System Basics/list.js
@@ -1,3 +1,22 @@
 const fs = require('fs');
+
+/* The 'Sync' in readdirSync method means that 
+ * the process happens sychronously. This means
+ * that JS will stop what ever it's doing until
+ * it is done. */
+console.log('\nStarted reading logs...');
 const files = fs.readdirSync('./assets');
-console.log(files);
+console.log('Complete!');
+
+console.log(files,'\n');
+
+console.log('Started async reading files...');
+/* Once files in dir are read, they are passed to
+ * a callback function in second arg */
+fs.readdir('./assets', (err, files) => {
+
+    console.log('Completed async!');
+    console.log(files,'\n');
+
+});
+console.log('Async readdir is still running, but this is printed.');
