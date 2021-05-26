f25a4740897c81a08a45947ae125fe6c177a9358
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 14:12:01 -0400

    Added colorsRenamed.md

--- Node.js_Work/EssentialTraining/File System Basics/rename.js
+++ Node.js_Work/EssentialTraining/File System Basics/rename.js
@@ -3,8 +3,15 @@ const fs = require('fs');
 /* fs.rename and fs.renameSync
  *  fs.rename('currentPath', 'newPath', callback)
  * fs.rename can also be used to move files. */
-fs.rename('./assets/colors.md', './assets/storage/colorsMoved.md', (err) => {
+fs.rename('./assets/colors.md', './assets/colorsRenamed.md', (err) => {
     if (err){
         throw err;
     }
 })
+
+setTimeout(() => {
+
+    // fs.unlink removes files, has sync and async
+    fs.unlink('./assets/colo')
+
+}, 4000);
