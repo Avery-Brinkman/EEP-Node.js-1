5802bf634f5ad64280146dde78046d1cd4ed191f
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 12:29:35 -0400

    Added emitters and handlers

---
+++ Node.js_Work/EssentialTraining/events.js
@@ -0,0 +1,13 @@
+const events = require('events');
+
+const emitter = new events.EventEmitter();
+ 
+/* Handler
+ * emitter.on(nameToListenFor, callbackToRun) */
+emitter.on('customEvent', (message, user) => {
+    console.log(`${user}:${message}`)
+})
+
+// emitter.emit('name', dataToBePassedToHandler)
+emitter.emit('customEvent', 'Hello World!', 'Computer');
+emitter.emit('customEvent', 'yo im live', 'Matt');
