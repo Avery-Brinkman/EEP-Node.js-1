e94c28be2b022a493906140801e55eb89547bbec
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-26 14:00:46 -0400

    Created append.js

---
+++ Node.js_Work/EssentialTraining/File System Basics/append.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+// Don't need anything extra to read JSON files
+const colorData = require('./assets/colors.json');
+
+colorData.colorList.forEach((c) => {
+
+    /* fs.appendFile will create files that don't
+     * already exist. */
+    fs.appendFile('./assets/colors.md', `${c.name}: ${c.hex}\n`);
+})
