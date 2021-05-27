07e37e0ab99b5e705f6ffaf3eed22903726eadb6
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-27 14:04:46 -0400

    Runs readStream.js thru PowerShell

--- Node.js_Work/EssentialTraining/File System Basics/Files and Streams/exec.js
+++ Node.js_Work/EssentialTraining/File System Basics/Files and Streams/exec.js
@@ -1,7 +1,26 @@
 // https://skimfeed.com/blog/windows-command-prompt-ls-equivalent-dir/
 
 const cp = require('child_process');
+const { stderr } = require('process');
 
-//cp.exec('explorer http://www.google.com/')
+console.log('\n\n');
 
-cp.exec('start PowerShell')
+// cp.exec('explorer http://www.google.com/')
+
+// cp.exec('start PowerShell')
+
+cp.exec('dirt', (err, data, stderr) => {
+    // err is the VSC error
+    // stderr is the PowerShell error (or UNIX Bash or Linux Command Line)
+    if (err) {
+        console.log(stderr);
+    }
+    console.log(data);
+})
+
+cp.exec('node ./readStream.js', (err, data, stderr) => {
+    if (err) {
+        console.log(stderr);
+    }
+    console.log(data);
+})
