c785d72f96d2cc9c55f5fe8054e18764d42da77f
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-27 13:52:50 -0400

    Fixed (UNIX to Windows)

--- Node.js_Work/EssentialTraining/File System Basics/Files and Streams/exec.js
+++ Node.js_Work/EssentialTraining/File System Basics/Files and Streams/exec.js
@@ -1,3 +1,7 @@
+// https://skimfeed.com/blog/windows-command-prompt-ls-equivalent-dir/
+
 const cp = require('child_process');
 
-cp.exec('explorer http://www.google.com/')
+//cp.exec('explorer http://www.google.com/')
+
+cp.exec('start PowerShell')
