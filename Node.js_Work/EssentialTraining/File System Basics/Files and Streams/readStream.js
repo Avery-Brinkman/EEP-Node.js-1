8e78aeb68c107593a556cc6675ee79f4e6f877ac
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-27 13:09:24 -0400

    Reads file w/ readStream

--- Node.js_Work/EssentialTraining/File System Basics/Files and Streams/readStream.js
+++ Node.js_Work/EssentialTraining/File System Basics/Files and Streams/readStream.js
@@ -1,9 +1,18 @@
 const fs = require('fs')
 
-const readStream = fs.createReadStream(); 
+const readStream = fs.createReadStream('./assets/lorum-ipsum.md', 'utf-8'); 
 
 console.log('Type something...');
+readStream.on('data', (data) => {
+    console.log(`I read ${data.length - 1} characters of text.`);
+})
 
-process.stdin.on('data', (data) => {
-    console.log(`I read ${data.length - 2} characters of text.`);
+/* readStream reads bit by bit. This uses less
+ * memory. Instead of reading everything all at
+ * once and loading into a buffer, you're reading
+ * bit by bit and chunk by chunk. They also raise
+ * events, which gives us a little more control. */
+
+readStream.on('end', () => {
+    console.log('Done reading file.');
 })
